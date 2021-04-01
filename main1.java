ca3c3c48d3855fb84138873259828e2371d4f1ba
z-xcm <zxcm0010@gmail.com>
2021-04-01 11:20:23 +0900

    1~100 sum (수업)

---
+++ main1.java
@@ -0,0 +1,13 @@
+public class OneTo100Sum {
+	public static void main(String[] args) {
+		
+		int i = 1;
+		int sum = 0;
+		
+		for(i = 1; i <= 100; i++){
+			sum += i;
+		}
+		
+		System.out.println(sum);
+	}
+}
